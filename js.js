5400f72e0560e5b33de1a7e9159f7d5cc3e1c296
Ahmed00github <110639451+Ahmed00github@users.noreply.github.com>
2022-09-26 19:12:29 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,4 @@
+let p_copy                      = document.querySelector(".p_copy");
+let dateNow                     = new Date();
+let year                        = dateNow.getFullYear();
+p_copy.innerHTML=`&#169;Ahmed khaled ${year}`;
